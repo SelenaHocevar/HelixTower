2c62d69b3ee34bc9b959e4a7c16b821ce4d2a5ec
Selena Hocevar <selena.hocevar@gmail.com>
2019-12-24 15:27:05 +0100

    Napisane su funkcije za nove vrste zidova i prepreka

--- src/funkcije.h
+++ src/funkcije.h
@@ -5,6 +5,9 @@ void set_normal_and_vertex(float u,float v);
 void set_normal_and_vertexSphere(float u, float v);
 void draw_Sphere();
 void draw_Cylinder();
-void nacrtaj_horizontalni_zidic(int i);
+void nacrtaj_horizontalnu_prepreku(int i,float pocetak_prepreke,float kraj_prepreke);
+void nacrtaj_vertikalnu_prepreku(int i);
+void nacrtaj_zid_1(float angle,float kraj_zida,int i);
+void nacrtaj_zid_2(float angle,float kraj_zida,int i);
 
 #endif
--- src/prepreke.c
+++ src/prepreke.c
@@ -7,32 +7,81 @@
 #define EPSILON 0.01
 
 
-GLfloat ambient_coeffs[] = { 0.9, 0.5, 0.2, 1 };
-GLfloat diffuse_coeffs[] = { 0.9, 0, 0, 1 };
+GLfloat ambient_coeffs[] = { 0.5, 0.2, 0.2, 1 };
+GLfloat diffuse_coeffs[] = { 0.5, 0.2, 0.2, 1 };
 GLfloat specular_coeffs[] = { 1, 0, 0, 1 };
 GLfloat shininess = 3;
 
-void nacrtaj_horizontalni_zidic(int i){
+
+void nacrtaj_zid_1(float angle,float kraj_zida,int i){
+
+  glPushMatrix();
+
+  glRotatef(angle*abs(i),0,1,0);
+
+  glBegin(GL_TRIANGLE_FAN);
+
+  glVertex3f(0,i,0);
+  for (float v = 0; v <= kraj_zida; v += PI / 20)
+          glVertex3f(sin(v)*2.5,i,cos(v)*2.5);
+  glEnd();
+  glPopMatrix();
+}
+
+
+void nacrtaj_zid_2(float angle,float kraj_zida,int i){
+
+  glPushMatrix();
+
+  glRotatef(angle*abs(i),0,1,0);
+
+  glBegin(GL_TRIANGLE_FAN);
+
+  glVertex3f(0,i,0);
+  for (float v = 0; v <= 5*PI/6; v += PI / 20) {
+          glVertex3f(sin(v)*2.5,i,cos(v)*2.5);
+  }
+  glEnd();
+
+  glBegin(GL_TRIANGLE_FAN);
+  glVertex3f(0,i,0);
+  for (float v = PI; v <= kraj_zida; v += PI / 20) {
+          glVertex3f(sin(v)*2.5,i,cos(v)*2.5);
+  }
+  glEnd();
+
+  glPopMatrix();
+}
+
+
+void nacrtaj_horizontalnu_prepreku(int i,float pocetak_prepreke,float kraj_prepreke){
     glPushMatrix();
-        
+
         glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
         glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
         glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
         glMaterialf(GL_FRONT, GL_SHININESS, shininess);
         glShadeModel(GL_SMOOTH);
-        
+
         glRotatef(30*abs(i),0,1,0);
-        
-        
+
+
         glBegin(GL_TRIANGLE_FAN);
             glVertex3f(0,i+0.01,0);
-            for (float v = 0; v <= PI*2 + EPSILON; v += PI / 20) {
-                
-                if(v>=PI+PI/2 && v<=PI+5*PI/6)
+            for (float v = pocetak_prepreke; v <= kraj_prepreke+ EPSILON; v += PI / 20) {
                     glVertex3f(sin(v)*2.5,i+0.01,cos(v)*2.5);
             }
-            
+
         glEnd();
-        
+
     glPopMatrix();
+}
+
+void nacrtaj_vertikalnu_prepreku(int i){
+  glPushMatrix();
+    glTranslatef(1.5,i+0.4,1);
+    glRotatef(-45,0,1,0);
+    glScalef(1.5,1,0.2);
+    glutSolidCube(0.8);
+  glPopMatrix();
 }
