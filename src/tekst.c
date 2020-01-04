2c3b4ea471a9d788cd33322db93b60beebe30efd
Selena Hocevar <selena.hocevar@gmail.com>
2020-01-04 22:23:00 +0100

    Dodata je funkcija za ispisivanje poruke kada se predje igrica

--- src/tekst.c
+++ src/tekst.c
@@ -7,24 +7,26 @@
 #define EPSILON 0.01
 
 
-//Funkcija za ispisivanje trenutnog rezultata
+/* Funkcija za ispisivanje trenutnog rezultata */
 void ispisi_rezultat(int score){
+    
+    /* Postavlja se boja teksta */
     glColor3f(0.9, 0.1, 0.1);
  
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0, 300, 0, 300);
+    gluOrtho2D(0, 2000, 0, 2000);
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
     glLoadIdentity();
 
-
+    /*Pravimo string sa rezultatom*/
     char score_s[50];
     sprintf(score_s,"Score:  %d",score);
 
-   
-    glRasterPos2i(70, 280);
+    /*Pozicioniramo i ispisujemo taj string*/
+    glRasterPos2i(200, 1800);
     glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,
                  score_s);
     
@@ -39,8 +41,6 @@ void game_over(float y_loptica){
     
     glPushMatrix();
     
-    glColor3f(1,1,1);
-    
     glPushMatrix();
     
     //Pravougaonik sa porukom se pozicionira na sredinu
@@ -48,6 +48,7 @@ void game_over(float y_loptica){
     glScalef(0.5,0.3,1);
     
     //Crta se beli pravougaonik na kojem ce se nalaziti tekst
+    glColor3f(1,1,1);
     glBegin(GL_POLYGON);
     glVertex3f(-2,y_loptica+2,1);
     glVertex3f(2,y_loptica+2,1);
@@ -73,15 +74,41 @@ void game_over(float y_loptica){
     glMatrixMode(GL_PROJECTION);
     glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(0, 400, 0, 400);
+    gluOrtho2D(0, 2000, 0, 2000);
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
     glLoadIdentity();
 
-   
-    glRasterPos2i(170, 280);
+    //Ispisuje se poruka
+    glRasterPos2i(850, 1350);
     glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,
                  "   GAME OVER\nPress R to try again!");
+    glPopMatrix();
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glPopMatrix();
+}
+
+void you_win(float y_loptica){
+    
+    glPushMatrix();
+    
+    //Postavlja se boja teksta
+    glColor3f(1, 1, 1);
+    
+ 
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    gluOrtho2D(0, 2000, 0, 2000);
+    glMatrixMode(GL_MODELVIEW);
+    glPushMatrix();
+    glLoadIdentity();
+
+    //Ispisuje se poruka
+    glRasterPos2i(860, 650);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,
+                 "Congratulations!\n    YOU WIN!");
     glPopMatrix();
     glMatrixMode(GL_PROJECTION);
     glPopMatrix();
